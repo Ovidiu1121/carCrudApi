110428803150316132c7d5ed8df7bc474d80f049
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-04-20 20:25:50 +0300

    first commit

---
+++ CarCrudApi/Cars/Repository/interfaces/ICarRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarCrudApi.Cars.Model;
+using CarCrudApi.Dto;
+
+namespace CarCrudApi.Cars.Repository.interfaces
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAllAsync();
+        Task<Car> GetByBrandAsync(string brand);
+        Task<Car> GetByIdAsync(int id);
+        Task<Car> CreateCar(CreateCarRequest request);
+        Task<Car> UpdateCar(int id,UpdateCarRequest request);
+        Task<Car> DeleteCarById(int id);
+
+    }
+}
